a12196e33a410794b81f640b5de0780da18b2cf8
90538 <90538@AHMEET>
2023-02-18 02:03:16 +0300

    Add project files.

---
+++ WhatsappMessageSenderWindowsForm/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace WhatsappMessageSenderWindowsForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.richtxt_Message = new System.Windows.Forms.RichTextBox();
+            this.txt_Amount = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_Stop = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_SleepTime = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.BackColor = System.Drawing.Color.Transparent;
+            this.btn_Start.ForeColor = System.Drawing.Color.Black;
+            this.btn_Start.Location = new System.Drawing.Point(303, 263);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(75, 23);
+            this.btn_Start.TabIndex = 0;
+            this.btn_Start.Text = "Start";
+            this.btn_Start.UseVisualStyleBackColor = false;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // richtxt_Message
+            // 
+            this.richtxt_Message.Location = new System.Drawing.Point(180, 124);
+            this.richtxt_Message.Name = "richtxt_Message";
+            this.richtxt_Message.Size = new System.Drawing.Size(198, 119);
+            this.richtxt_Message.TabIndex = 1;
+            this.richtxt_Message.Text = "";
+            // 
+            // txt_Amount
+            // 
+            this.txt_Amount.Location = new System.Drawing.Point(180, 25);
+            this.txt_Amount.Name = "txt_Amount";
+            this.txt_Amount.Size = new System.Drawing.Size(100, 22);
+            this.txt_Amount.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Black", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Gold;
+            this.label1.Location = new System.Drawing.Point(67, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Amount:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Black", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Gold;
+            this.label2.Location = new System.Drawing.Point(67, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Message:";
+            // 
+            // btn_Stop
+            // 
+            this.btn_Stop.BackColor = System.Drawing.Color.Transparent;
+            this.btn_Stop.ForeColor = System.Drawing.Color.Black;
+            this.btn_Stop.Location = new System.Drawing.Point(180, 263);
+            this.btn_Stop.Name = "btn_Stop";
+            this.btn_Stop.Size = new System.Drawing.Size(75, 23);
+            this.btn_Stop.TabIndex = 0;
+            this.btn_Stop.Text = "Stop";
+            this.btn_Stop.UseVisualStyleBackColor = false;
+            this.btn_Stop.Click += new System.EventHandler(this.btn_Stop_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Black", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.Gold;
+            this.label3.Location = new System.Drawing.Point(25, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 23);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Time Between";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Black", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.Gold;
+            this.label4.Location = new System.Drawing.Point(30, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(131, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Each Message:";
+            // 
+            // txt_SleepTime
+            // 
+            this.txt_SleepTime.Location = new System.Drawing.Point(180, 87);
+            this.txt_SleepTime.Name = "txt_SleepTime";
+            this.txt_SleepTime.Size = new System.Drawing.Size(100, 22);
+            this.txt_SleepTime.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Black", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.Gold;
+            this.label5.Location = new System.Drawing.Point(286, 87);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(131, 19);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "(1 recommended)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(459, 320);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_SleepTime);
+            this.Controls.Add(this.txt_Amount);
+            this.Controls.Add(this.richtxt_Message);
+            this.Controls.Add(this.btn_Stop);
+            this.Controls.Add(this.btn_Start);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Whatsapp Message Sender";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.RichTextBox richtxt_Message;
+        private System.Windows.Forms.TextBox txt_Amount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_Stop;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_SleepTime;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WhatsappMessageSenderWindowsForm/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using WindowsInput;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WhatsappMessageSenderWindowsForm
+{
+    public partial class Form1 : Form
+    {
+        bool check = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txt_Amount.Text == "")
+                {
+                    MessageBox.Show("Please enter a message amount", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else if (richtxt_Message.Text == "")
+                {
+                    MessageBox.Show("Please enter a message", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                
+                else if (txt_SleepTime.Text == "")
+                {
+                    MessageBox.Show("Please enter a sleep time", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                
+                else
+                {
+                    List<string> messageList = new List<string>();
+                    messageList.AddRange(richtxt_Message.Lines);
+                    MessageBox.Show("Message sender will start in 5 seconds\nPlease open WhatsappWeb","Information",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                    Thread.Sleep(5000);
+                    StartSending(messageList);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("An error occured. Please try again","Error", MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+
+        private void StartSending(List<string> list)
+        {
+            try
+            {
+                int i = 0;
+                string formattedSleepTimeText = txt_SleepTime.Text.Replace('.', ',');
+                double sleepTime = Convert.ToDouble(formattedSleepTimeText) * 1000;
+
+
+                int messageAmount = int.Parse(txt_Amount.Text);
+                int ma = messageAmount;
+                while (check && messageAmount != 0)
+                {
+                    InputSimulator inputSimulator = new InputSimulator();
+                    inputSimulator.Keyboard.TextEntry(list[i]);
+                    Thread.Sleep(Convert.ToInt32(sleepTime));
+                    inputSimulator.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.RETURN);
+                    messageAmount--;
+
+                    if (i < list.Count - 1)
+                    {
+                        i++;
+                    }
+                    else
+                    {
+                        i = 0;
+                    }
+
+                }
+                MessageBox.Show("Successfully sent " + ma.ToString() + " messages");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occured. Please try again", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+     
+        }
+
+        private void btn_Stop_Click(object sender, EventArgs e)
+        {
+            check = false;
+        }
+    }
+}
